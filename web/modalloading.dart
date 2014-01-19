9e0b43256d7f521c2818dbfa158c3b5c8e23eed8
nickfloros <nickfloros@gmail.com>
2014-01-19 14:13:45 +0000

    fixed modal

--- web/modalloading.dart
+++ web/modalloading.dart
@@ -1,4 +1,5 @@
 library modalloading;
+import 'dart:html';
 import 'package:polymer/polymer.dart';
 
 /**
@@ -8,28 +9,45 @@ import 'package:polymer/polymer.dart';
 class ModalLoading extends PolymerElement {
  
   bool applyAuthorStyles = true;
+  
+  @published bool fadeBackdrop=true;
+  DivElement _modalBackdrop ;
+  
   ModalLoading.created() : super.created() {
+    _modalBackdrop = new DivElement();
+    _modalBackdrop.classes.addAll(["modal-backdrop","fade","in"]);
   }
 
+  void enteredView() {
+    super.enteredView();
+  }
+  
   void show() {
     $['myModal'].hidden=false;
     $['myModal'].classes.toggle('in');
     $['myModal'].attributes['aria-hidden']='true';
     $['myModal'].style.display='block';
+    
+    if (fadeBackdrop)
+      ownerDocument.documentElement.children.add(_modalBackdrop);
   }
   
   void onClose() {
-    $['myModal'].classes.toggle('in');
-    $['myModal'].attributes['aria-hidden']='true';
-    $['myModal'].style.display='none';
-    $['myModal'].hidden=true;
+    _hide();
   }
   
   void onSave() {
+    _hide();
+  }
+  
+  void _hide() {
     $['myModal'].classes.toggle('in');
     $['myModal'].attributes['aria-hidden']='false';
     $['myModal'].style.display='none';
     $['myModal'].hidden=true;
+
+    if (fadeBackdrop)
+      ownerDocument.documentElement.children.removeLast();
   }
 }
 
